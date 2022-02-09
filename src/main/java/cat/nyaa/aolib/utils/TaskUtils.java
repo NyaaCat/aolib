63f2109e86ff52df1fcc424202b795c5c23cbf9f
Takaranoao <takaranoao@gmail.com>
2022-02-10 00:34:39 +0800

    TaskUtils

---
+++ src/main/java/cat/nyaa/aolib/utils/TaskUtils.java
@@ -0,0 +1,52 @@
+package cat.nyaa.aolib.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class TaskUtils {
+    public static int get5BitId(@NotNull UUID uniqueId) {//MAX= 0001 1111 =31
+        return getBitId(uniqueId, 5);
+    }
+
+    public static int getBitId(@NotNull UUID uniqueId, int bit) {
+        long l = (uniqueId.getMostSignificantBits() >> 1) + (uniqueId.getLeastSignificantBits() >> 1);
+        return (int) ((l >>> (64 - bit)));
+    }
+
+    public static void waitToRun(long tickCount, int bit, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        int mod = 1 << bit;
+        int hashedId = getBitId(uniqueId, bit);
+        if (hashedId == (tickCount % mod)) {
+            runnable.run();
+        }
+    }
+
+    public static void mod128TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 7, uniqueId, runnable);
+    }
+
+    public static void mod64TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 6, uniqueId, runnable);
+    }
+
+    public static void mod32TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 5, uniqueId, runnable);
+    }
+
+    public static void mod16TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 4, uniqueId, runnable);
+    }
+
+    public static void mod8TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 3, uniqueId, runnable);
+    }
+
+    public static void mod4TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 2, uniqueId, runnable);
+    }
+
+    public static void mod2TickToRun(long tickCount, @NotNull UUID uniqueId, @NotNull Runnable runnable) {
+        waitToRun(tickCount, 1, uniqueId, runnable);
+    }
+}
