ed2d246dde00486440466045ac56a04323cdb737
Takaranoao <takaranoao@gmail.com>
2022-03-01 02:06:56 +0800

    fix(CommandUiItem): Fix invalid function field and make the create function static

--- src/main/java/cat/nyaa/aolib/aoui/item/CommandUiItem.java
+++ src/main/java/cat/nyaa/aolib/aoui/item/CommandUiItem.java
@@ -1,35 +1,50 @@
 package cat.nyaa.aolib.aoui.item;
 
+import cat.nyaa.aolib.AoLibPlugin;
 import cat.nyaa.aolib.aoui.UIManager;
 import cat.nyaa.aolib.network.data.DataClickType;
 import cat.nyaa.aolib.utils.RunCommandUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 
 public class CommandUiItem implements IClickableUiItem, IUiItem {
+    @Nullable
     private final String commandPermission;
+    @NotNull
     private final String command;
-    private final Player player;
+    @Nullable
+    private final Player sender;
+    @NotNull
     private final Plugin plugin;
+    @Nullable
     private final UIManager uiManager;
+    @NotNull
     private final ItemStack holdItem;
 
-    public CommandUiItem(Plugin plugin, ItemStack holdItem, @Nullable UIManager uiManager, Player player, String command, @Nullable String commandPermission) {
+    public CommandUiItem(@NotNull Plugin plugin, @NotNull ItemStack holdItem, @Nullable UIManager uiManager, @Nullable Player sender, @NotNull String command, @Nullable String commandPermission) {
         this.plugin = plugin;
         this.holdItem = holdItem;
         this.uiManager = uiManager;
-        this.player = player;
+        this.sender = sender;
         this.command = command;
         this.commandPermission = commandPermission;
+    }
+
+    @Nullable
+    public static CommandUiItem create(ItemStack holdItem, @Nullable UIManager uiManager, String command, @Nullable String commandPermission) {
+        if (AoLibPlugin.instance == null) return null;
+        return new CommandUiItem(AoLibPlugin.instance, holdItem, uiManager, null, command, commandPermission);
     }
 
     @Override
     public void onClick(DataClickType clickType, Player player) {
         if (clickType.equals(DataClickType.PICKUP)) {
-            RunCommandUtils.executeCommand(player, command, commandPermission, plugin);
+            if (command.isEmpty()) return;
+            RunCommandUtils.executeCommand(sender == null ? player : sender, command, commandPermission, plugin);
             if (uiManager != null) {
                 uiManager.sendCloseWindow(player);
             }
